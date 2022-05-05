b861b2618631123d50a0056c7183593998cfe83d
Shori <s.horii@d-1.dev>
2022-05-05 12:59:27 +0900

    upload to github

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+// CONTRACT_ADDRESSに、コントラクトアドレスを保存します。
+const CONTRACT_ADDRESS = "0x3051cDA79395fD97bc1dEC049B33D7321E2226E6";
+
+//NFT キャラクター の属性をフォーマットしてオブジェクトとして返します。
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+// 変数を constants.js 以外の場所でも使えるようにします。
+export { CONTRACT_ADDRESS, transformCharacterData };
